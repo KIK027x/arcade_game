2ee4db37c4891c2bd3ac25b8016a24287ac97255
KIK027x <aleksei.kof@mail.ru>
2026-01-23 16:52:58 +0500

    Update level.py

--- level.py
+++ level.py
@@ -3,17 +3,13 @@ from arcade import load_tilemap
 
 class Level:
     def __init__(self, map_path):
-        # Загружаем карту
         self.tile_map = load_tilemap(map_path)
         self.scene = self.tile_map.sprite_lists
 
-        # Получаем тайловый слой Platforms как список спрайтов
         self.platforms = self.scene.get("Platforms", arcade.SpriteList())
-
-        # Отладка
-        print("Платформы:", len(self.platforms))
+        self.coins = self.scene.get("coins", arcade.SpriteList())
+        self.end_flag = self.scene.get("EndFlag", arcade.SpriteList())
 
     def draw(self):
-        # Рисуем всё
         for sprite_list in self.scene.values():
             sprite_list.draw()
